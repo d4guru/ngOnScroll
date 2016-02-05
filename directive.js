79bf59befe2a32baae86a934148681bb3010150c
Lorenzo <l.cambria@gmail.com>
2016-02-05 22:20:33 +0100

    Update directive.js

--- directive.js
+++ directive.js
@@ -1,23 +1,23 @@
-app.directive( 'ngOnScroll', ['$timeout', function($timeout) {
-    return {
-        restrict:'A',
-        scope:{
-            onScroll:'&',
-            onScrollDistance : '='
-        },
-        link : function(scope, elm, attr) {
-            var onScrollDistance = (scope.onScrollDistance * 100) || 0;
-            $timeout( function (){
-                console.log("onScrollDistance : ", onScrollDistance);
-                window.onscroll = function (event) {
-                    var clientHeight = document.documentElement.clientHeight;
-                    var clientRect = document.body.getBoundingClientRect();
-                    if( (clientHeight + onScrollDistance) >= clientRect.bottom){
-                        scope.onScroll()
-                    }
-                }
 
-            }, 0, false);
+angular.module('ng-on-scroll', [])
+    .directive( 'ngOnScroll', ['$timeout', function($timeout) {
+        return {
+            restrict:'A',
+            scope:{
+                onScroll:'&',
+                onScrollDistance : '='
+            },
+            link : function(scope, elm, attr) {
+                var onScrollDistance = (scope.onScrollDistance * 100) || 0;
+                $timeout( function (){
+                    window.onscroll = function (event) {
+                        var clientHeight = document.documentElement.clientHeight;
+                        var clientRect = document.body.getBoundingClientRect();
+                        if( (clientHeight + onScrollDistance) >= clientRect.bottom){
+                            scope.onScroll()
+                        }
+                    }
+                }, 0, false);
+            }
         }
-    }
-}]);
+    }]);
